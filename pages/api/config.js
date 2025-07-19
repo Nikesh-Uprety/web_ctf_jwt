81b2abe46df461bc03f4928a7986c5f6b4c1768c
Nikesh Uprety <upretynikesh123@gmail.com>
2025-07-19 22:50:15 +0545

    some changes

--- pages/api/config.js
+++ pages/api/config.js
@@ -3,8 +3,8 @@ export default function handler(req, res) {
     if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
 
         res.status(200).json({
-            secret: "UEdTX0YzUEUzR19YM0w =",
-            message: "Nice finddd! Use this to sign admin JWTs"
+            secret: "TkNBX0BETUlOX1MzQ1JFVF9LM1k=",
+            message: "Nice find! Now you know what to do!"
         });
     } 
 
